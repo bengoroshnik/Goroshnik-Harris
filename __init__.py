e70ecf54ea599e6cacff9a6650e428a596e59c7d
bengoroshnik <38156753+bengoroshnik@users.noreply.github.com>
2021-11-06 11:21:34 -0400

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# init.py
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# init SQLAlchemy so we can use it later in our models
+db = SQLAlchemy()
